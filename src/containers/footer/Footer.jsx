57fff3152670c4629b1aa5b38edba8b9be216a7e
Ekweaga <hikay133@gmail.com>
2022-07-15 21:10:47 +0100

    UIUX figma website

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import gpt3Logo from '../../logo.svg';
+
 import './footer.css';
 
 const Footer = () => (
@@ -14,7 +14,7 @@ const Footer = () => (
 
     <div className="gpt3__footer-links">
       <div className="gpt3__footer-links_logo">
-        <img src={gpt3Logo} alt="gpt3_logo" />
+        <h1 style={{ color: 'white', fontWeight: '500' }}>C T S, building the future</h1>
         <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
       </div>
       <div className="gpt3__footer-links_div">
